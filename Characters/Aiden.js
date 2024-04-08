8c14064400486d773bd14072eb7c0c7df3a39208
EmiliaBanak <148707385+EmiliaBanak@users.noreply.github.com>
2024-04-08 13:42:31 +0200

    Update Aiden.js

--- Characters/Aiden.js
+++ Characters/Aiden.js
@@ -2,60 +2,60 @@ function setup() {
   createCanvas(500, 500);
 }
 function draw() {
-  background(120, 132, 5);
-  //girl look
+  background(120,132,5);
+ //Aiden look
   fill(0);
   strokeWeight(0);
-  //tshirt
-  fill('pink');
+  //body
   rect(200, 200, 20, 14);
+
   //head
-  fill(255, 255, 255);
-  rect(200, 185, 20, 15);
+  fill(255,255,255);
+  rect(200,185,20,15);
   //legs
-  rect(200, 220, 5, 10);
-  rect(215, 220, 5, 10);
+  rect(200,220,5,10);
+  rect(215,220,5,10);
   //hands
-  rect(197, 205, 3, 10);
-  rect(220, 205, 3, 10);
+  rect(197,205,3,10);
+  rect(220,205,3,10);
   //shoes
-  fill('pink');
-  rect(200, 226, 5, 5);
-  rect(215, 226, 5, 5);
-  //skirt
-  fill('pink');
-  rect(200, 214, 20, 7);
+  fill(0);
+  rect(200,226,5,5);
+  rect(215,226,5,5);
+  //pants
+  fill(0,102,51);
+  rect(200,214,20,4);
+  rect(200,218,9,3);
+  rect(211,218,9,3);
   //eyes
-  fill('blue');
-  rect(204, 190, 2, 2);
-  rect(214, 190, 2, 2);
+  fill(0);
+  rect(204,190,2,2);
+  rect(214,190,2,2);
   //hair
-  fill('yellow');
-  rect(199, 185, 3, 5);
-  rect(218, 185, 3, 5);
-  rect(214, 185, 4, 4);
-  rect(201, 185, 4, 4);
-  //buns
-  rect(195, 178, 10, 8);
-  rect(215,178,10,8);
-  //hair top
-  fill("yellow");
-  rect(200, 180, 18, 7);
+  rect(199,185,3,5);
+  rect(218,185,3,5);
+  rect(214,185,4,4);
+  rect(201,185,4,4);
+  //hat
+  fill('red');
+  rect(200,180,20,4);
+  rect(199,183,10,4);
+  rect(211,183,10,4);
   //mouth
   fill(0);
-  rect(208, 195, 4, 1);
-  //sleeves
-  fill('pink');
-  rect(220, 202, 3, 6);
-  rect(197, 202, 3, 6);
-  //cheecks
-  fill(255, 190, 153);
-  rect(215, 194, 3, 3);
-  rect(202, 194, 3, 3);
-  //ears
-  fill("white");
-  rect(220, 191, 1, 3);
-  rect(199, 191, 1, 3);
+  rect(208,195,4,1);
+  //tshirt image
+  fill('yellow');
+  rect(206,204,2,2);
+  rect(207,204,2,7);
+  rect(210,204,4,2);
+  rect(210,206,2,2);
+  rect(212,207,1,1);
+  rect(212,208,1,2);
+  rect(210,209,3,2);
+//sleeves
+fill(0);
+rect(220,202,3,6);
+rect(197,202,3,6);
 }
-
 
