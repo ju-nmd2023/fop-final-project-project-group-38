7eac781e10433c1bf1101e30c47992cf284dc121
EmiliaBanak <148707385+EmiliaBanak@users.noreply.github.com>
2024-04-19 10:39:18 +0200

    have to add swing and plants,trees

--- Buildings/house-area.js
+++ Buildings/house-area.js
@@ -1,9 +1,12 @@
 function setup() {
   createCanvas(700, 500);
 }
+
 function draw() {
   background(120, 132, 5);
-  scale(1.3);
+  scale(1.2);
+  // Move the entire house 10 pixels to the right
+  translate(40, 0);
   //house
   fill(80, 61, 61);
   strokeWeight(0);
@@ -93,4 +96,131 @@ function draw() {
   rect(240, 84, 50, 4);
   fill(105, 105, 105);
   rect(230, 174, 70, 2);
+
+  translate(-50, 0);
+  //playground
+  fill(0);
+  rect(77, 240, 5, 90);
+  rect(122, 240, 5, 90);
+  rect(68, 235, 70, 5);
+  rect(77, 230, 5, 5);
+  rect(122, 230, 5, 5);
+  rect(80, 270, 53, 5);
+  rect(117, 225, 5, 5);
+  rect(112, 220, 5, 5);
+  rect(107, 215, 5, 5);
+  rect(102, 210, 5, 5);
+  rect(82, 225, 5, 5);
+  rect(87, 220, 5, 5);
+  rect(92, 215, 5, 5);
+  rect(97, 210, 5, 5);
+  fill("red");
+  rect(82, 240, 40, 30);
+  fill("grey");
+  rect(82, 230, 40, 5);
+  rect(87, 225, 30, 5);
+  rect(92, 220, 20, 5);
+  rect(97, 215, 10, 5);
+  //door
+  fill(0);
+  rect(96, 250, 10, 10);
+  //ladder
+  fill("yellow");
+  rect(92, 250, 5, 80);
+  rect(105, 250, 5, 80);
+  rect(95, 270, 10, 5);
+  rect(95, 280, 10, 5);
+  rect(95, 290, 10, 5);
+  rect(95, 300, 10, 5);
+  rect(95, 310, 10, 5);
+  rect(95, 320, 10, 5);
+  //slide
+  rect(130, 270, 5, 5);
+  rect(133, 275, 5, 5);
+  rect(135, 280, 5, 5);
+  rect(138, 285, 5, 5);
+  rect(141, 290, 5, 5);
+  rect(145, 295, 5, 5);
+  rect(148, 300, 5, 5);
+  rect(151, 305, 5, 5);
+  rect(155, 310, 5, 5);
+  rect(160, 315, 5, 5);
+  rect(165, 320, 5, 5);
+  rect(170, 325, 10, 5);
+
+  //GIRL
+  scale(0.7);
+  translate(-110, 240);
+  //girl look
+  fill(0);
+  strokeWeight(0);
+  //tshirt
+  fill("pink");
+  rect(200, 200, 20, 14);
+  //head
+  fill(255, 255, 255);
+  rect(200, 185, 20, 15);
+  //legs
+  rect(200, 220, 5, 10);
+  rect(215, 220, 5, 10);
+  //hands
+  rect(197, 205, 3, 10);
+  rect(220, 205, 3, 10);
+  //shoes
+  fill("pink");
+  rect(200, 226, 5, 5);
+  rect(215, 226, 5, 5);
+  //skirt
+  fill("pink");
+  rect(200, 214, 20, 7);
+  //eyes
+  fill("blue");
+  rect(204, 190, 2, 2);
+  rect(214, 190, 2, 2);
+  //hair
+  fill("yellow");
+  rect(199, 185, 3, 5);
+  rect(218, 185, 3, 5);
+  rect(214, 185, 4, 4);
+  rect(201, 185, 4, 4);
+  //buns
+  rect(195, 178, 10, 8);
+  rect(215, 178, 10, 8);
+  //hair top
+  fill("yellow");
+  rect(200, 180, 18, 7);
+  //mouth
+  fill(0);
+  rect(208, 195, 4, 1);
+  //sleeves
+  fill("pink");
+  rect(220, 202, 3, 6);
+  rect(197, 202, 3, 6);
+  //cheecks
+  fill(255, 190, 153);
+  rect(215, 194, 3, 3);
+  rect(202, 194, 3, 3);
+  //ears
+  fill("white");
+  rect(220, 191, 1, 3);
+  rect(199, 191, 1, 3);
+  //swing
+  translate(0, 0);
+  //plants
+  fill("green");
+  rect(370, -44, 60, 60);
+  fill("yellow");
+  rect(376, -30, 5, 5);
+  rect(415, -35, 5, 5);
+  rect(410, 0, 5, 5);
+  rect(380, -5, 5, 5);
+  //tree
+  fill("brown");
+  rect(290, -96, 20, 113);
+  fill("green");
+  rect(260, -130, 80, 80);
+  //path
+  fill(238, 232, 190);
+  rect(520, 18, 80, 340);
+  rect(120, 250, 850, 80);
 }
